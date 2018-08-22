75a109a47060570804c15622bd351ceb81e0e722
Katrina Pritchard <pritckat@gmail.com>
2018-08-22 20:03:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   end
 
   def kind
-    if @side_1 = 0 || @side_1 > @side_2 + @side_3 || @side_2 > @side_1 + @side_3 || @side_3 > @side_1 + @side_2
+    if @side_1 == 0 || @side_1 > @side_2 + @side_3 || @side_2 > @side_1 + @side_3 || @side_3 > @side_1 + @side_2
       raise TriangleError
     else
       if @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
